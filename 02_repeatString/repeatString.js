707e93f4e0a6948b08f681f729baf7b9290e1377
dangoodie <dan.gooden.dev@gmail.com>
2023-04-08 22:58:29 +1000

    completing 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function (string, count) {
+  let outputString = "";
+  if (count < 0) {
+    outputString = "ERROR";
+  } else {
+    for (let i = 0; i < count; i++) {
+      outputString += string;
+    }
+  }
+  return outputString;
 };
 
 // Do not edit below this line
