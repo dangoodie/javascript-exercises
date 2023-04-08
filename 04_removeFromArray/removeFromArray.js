1631ba30f872200dbc36f6656c1387d97de957a5
dangoodie <dan.gooden.dev@gmail.com>
2023-04-09 02:51:57 +1000

    completing 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (array, ...args) {
+  for (const argument of args) {
+    const index = array.indexOf(argument);
+    if (index >= 0) {
+      array.splice(index, 1);
+    }
+  }
+  return array;
 };
 
 // Do not edit below this line
