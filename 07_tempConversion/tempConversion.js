bb608c6a4f2688e8bd0b2c96dc8af0a6e8348a4f
dangoodie <dan.gooden.dev@gmail.com>
2023-04-10 12:35:17 +1000

    completing 07_tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,9 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fInput) {
+  return parseFloat(((fInput - 32) / 1.8).toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(cInput) {
+  return parseFloat((cInput * 1.8 + 32).toFixed(1));
 };
 
 // Do not edit below this line
