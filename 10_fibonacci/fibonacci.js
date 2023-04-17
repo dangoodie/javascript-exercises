be088ebbf729572ac165a4943932e675e05f8e33
dangoodie <dan.gooden.dev@gmail.com>
2023-04-17 22:47:34 +1000

    competing the fibonacci sequence exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,23 @@
-const fibonacci = function() {
+const fibonacci = function (input) {
 
+  // parse to accept strings
+  input = parseInt(input);
+  let n1 = 0,
+    n2 = 1,
+    finalOutput = 1;
+
+
+  // check for negative numbers
+  if (input < 1) return "OOPS";
+
+  // generate fibonacci to specified input length
+  for (let i = 1; i < input; i++) {
+    finalOutput = n1 + n2;
+    n1 = n2;
+    n2 = finalOutput;
+  }
+
+  return finalOutput;
 };
 
 // Do not edit below this line
