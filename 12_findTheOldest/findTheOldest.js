784e609d53d5fc6c5f1784052447b764a9a83df4
dangoodie <dan.gooden.dev@gmail.com>
2023-04-17 23:13:50 +1000

    completing findTheOldest exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-const findTheOldest = function() {
+const findTheOldest = function (obj) {
+  currentYear = new Date().getFullYear();
 
+  const oldest = obj.sort((a, b) => {
+    if (!a.yearOfDeath) a.yearOfDeath = currentYear;
+    if (!b.yearOfDeath) b.yearOfDeath = currentYear;
+
+    const lastGuy = a.yearOfDeath - a.yearOfBirth;
+    const nextGuy = b.yearOfDeath - b.yearOfBirth;
+    return lastGuy > nextGuy ? -1 : 1;
+  });
+
+  return oldest[0];
 };
 
 // Do not edit below this line
