b623ff9c1f41135ebbfd0c552f0e7f33bb8a0e92
dangoodie <dan.gooden.dev@gmail.com>
2023-04-17 22:32:43 +1000

    solving palindromes!

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,19 @@
-const palindromes = function () {
+const palindromes = function (string) {
+  //takes everything to lowercase, removes punctuation, and removes whitespace
+  cleanString = string
+    .toLowerCase()
+    .replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, "")
+    .replace(/\s+/g, "");
 
+  //creates a reversed string
+  reverseString = cleanString.split("").reverse().join("");
+
+  //checks for palindromes
+  if (cleanString === reverseString) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
 // Do not edit below this line
